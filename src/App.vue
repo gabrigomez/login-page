b03e9dc9f99318f2f8881243e66326223b2509f0
Gabriel <gabrigomez15@gmail.com>
2022-11-17 15:53:56 -0300

    feat: first components

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,20 @@
 <template>
   <div id="app">
-    <nav class="navbar">
-      <div class="container">
-        <a href="#" class="navbar-brand">Home</a>
-        <div>
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a href="#" class="nav-link">Login</a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">Sign Up</a>                    
-            </li>        
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <body>
-    <div class="login-dark">
-        <form method="post">
-            <h2 class="sr-only">Login Form</h2>
-            <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
-            <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email"></div>
-            <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
-            <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Log In</button></div><a href="#" class="forgot">Forgot your email or password?</a></form>
-    </div>    
-  </body>  
+    <NavBar />
+    <Login /> 
   </div>
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue'
+import Login from './components/Login.vue'
 
 export default {
   name: 'App',
+  components: {
+    NavBar,
+    Login
+  }
   
 }
 </script>
@@ -40,77 +23,4 @@ export default {
   .navbar {
     background: #1e2833
   }
-
-    .login-dark {
-    height:1000px;
-    background:#475d62 url(https://wallpapercave.com/wp/wp2567217.jpg);
-    background-size:cover;
-    position:relative;
-  }
-
-  .login-dark form {
-    max-width:320px;
-    width:90%;
-    background-color:#1e2833;
-    padding:40px;
-    border-radius:4px;
-    transform:translate(-50%, -50%);
-    position:absolute;
-    top:50%;
-    left:50%;
-    color:#fff;
-    box-shadow:3px 3px 4px rgba(0,0,0,0.2);
-  }
-
-  .login-dark .illustration {
-    text-align:center;
-    padding:15px 0 20px;
-    font-size:100px;
-    color:#2980ef;
-  }
-
-  .login-dark form .form-control {
-    background:none;
-    border:none;
-    border-bottom:1px solid #434a52;
-    border-radius:0;
-    box-shadow:none;
-    outline:none;
-    color:inherit;
-  }
-
-  .login-dark form .btn-primary {
-    background:#214a80;
-    border:none;
-    border-radius:4px;
-    padding:11px;
-    box-shadow:none;
-    margin-top:26px;
-    text-shadow:none;
-    outline:none;
-  }
-
-  .login-dark form .btn-primary:hover, .login-dark form .btn-primary:active {
-    background:#214a80;
-    outline:none;
-  }
-
-  .login-dark form .forgot {
-    display:block;
-    text-align:center;
-    font-size:12px;
-    color:#6f7a85;
-    opacity:0.9;
-    text-decoration:none;
-  }
-
-  .login-dark form .forgot:hover, .login-dark form .forgot:active {
-    opacity:1;
-    text-decoration:none;
-  }
-
-  .login-dark form .btn-primary:active {
-    transform:translateY(1px);
-  }
-
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <nav class="navbar">
+            <div class="container">
+              <a href="#" class="navbar-brand">Home</a>
+              <div>
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">Login</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#" class="nav-link">Sign Up</a>                    
+                  </li>        
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
